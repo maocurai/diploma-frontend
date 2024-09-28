1f99da8346151fd8e4cb5546b0b0ecdf7fe374ff
maocurai <mpatnica924@gmail.com>
2024-09-28 23:04:43 +0300

    configuration fixed

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,27 +1,31 @@
-import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
 import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
 import {provideClientHydration} from '@angular/platform-browser';
-import {HTTP_INTERCEPTORS} from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClient, HttpClientModule} from '@angular/common/http';
 import {HttpIntercepterBasicAuthService} from './service/http/http-intercepter-basic-auth.service';
+import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
+import {TranslateHttpLoader} from "@ngx-translate/http-loader";
 
-
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 export const appConfig: ApplicationConfig = {
-
 
   providers: [
     provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
-    // importProvidersFrom(
-    //   TranslateModule.forRoot({
-    //     loader: {
-    //       provide: TranslateLoader,
-    //       useFactory: HttpLoaderFactory,
-    //       deps: [HttpClient],
-    //     },
-    //   })
-    // ),
+    importProvidersFrom(
+      HttpClientModule,
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient],
+        },
+      })
+    ),
     provideClientHydration(),
     {
       provide: HTTP_INTERCEPTORS,
--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,8 @@
 import {bootstrapApplication} from '@angular/platform-browser';
 import {AppComponent} from './app/app.component';
-import {HttpClient, provideHttpClient} from "@angular/common/http";
-import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
-import {importProvidersFrom} from "@angular/core";
-import {TranslateHttpLoader} from "@ngx-translate/http-loader";
+import {appConfig} from "./app/app.config";
 
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
-}
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideHttpClient(),
-    importProvidersFrom(
-      TranslateModule.forRoot({
-        loader: {
-          deps: [HttpClient],
-          provide: TranslateLoader,
-          useFactory: HttpLoaderFactory,
-        },
-      })
-    ),
-  ],
-}).catch((err) => console.error(err));
+
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
 
